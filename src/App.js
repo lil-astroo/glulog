2bb99da871c3a44ef5e000b94d8362abfa2d8f66
mani rahmati <astrroeffect@gmail.com>
2025-10-08 15:13:57 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate, useRoutes } from 'react-router-dom'
 import { ToastContainer, Bounce } from 'react-toastify';
 import { Share, Smartphone, SquarePlus } from 'lucide-react';
+import OneSignal from 'react-onesignal';
 import Cookies from 'js-cookie';
 import Navbar from './components/Navbar';
 import routes from './router';
@@ -32,39 +33,49 @@ export default function App() {
     if (userAuth && loginPage) {
       navigate('/dashboard');
     }
+
+    OneSignal.init({
+      appId: "ec5b905f-0206-47b3-b852-4878364dcc63",
+      notifyButton: {
+        enable: true,
+      },
+    });
+
   }, [location, navigate]);
 
-  if (!isStandalone) {
-    return (
-      <div className='standalone-warning'>
-        <img src={`${process.env.PUBLIC_URL}/logo.png`} className='standalone-warning__logo' alt="" />
-        <h1 className='standalone-warning__title'>وب اپلیکیشن را دانلود کنید</h1>
-        <div className='standalone-warning__details'>
-          <div className='standalone-warning__box'>
-            <Share className='standalone-warning__icon' />
-            <div className='standalone-warning__text'>
-              <h2 className='standalone-warning__subtitle'>Share</h2>
-              <small>گزینه share را از نوار مرورگر خود انتخاب کنید</small>
-            </div>
-          </div>
-          <div className='standalone-warning__box'>
-            <SquarePlus className='standalone-warning__icon' />
-            <div className='standalone-warning__text'>
-              <h2 className='standalone-warning__subtitle'>Add To Home Screen</h2>
-              <small>از منو ظاهر شده این گزینه را پیدا و انتخاب کنید</small>
-            </div>
-          </div>
-          <div className='standalone-warning__box'>
-            <Smartphone className='standalone-warning__icon' />
-            <div className='standalone-warning__text'>
-              <h2 className='standalone-warning__subtitle'>Add</h2>
-              <small>بدون تغییر هیچ گزینه دکمه Add را بزنید</small>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // if (!isStandalone) {
+  //   return (
+  //     <div className='standalone-warning'>
+  //       <img src={`${process.env.PUBLIC_URL}/logo.png`} className='standalone-warning__logo' alt="" />
+  //       <h1 className='standalone-warning__title'>وب اپلیکیشن را دانلود کنید</h1>
+  //       <div className='standalone-warning__details'>
+  //         <div className='standalone-warning__box'>
+  //           <Share className='standalone-warning__icon' />
+  //           <div className='standalone-warning__text'>
+  //             <h2 className='standalone-warning__subtitle'>Share</h2>
+  //             <small>گزینه share را از نوار مرورگر خود انتخاب کنید</small>
+  //           </div>
+  //         </div>
+  //         <div className='standalone-warning__box'>
+  //           <SquarePlus className='standalone-warning__icon' />
+  //           <div className='standalone-warning__text'>
+  //             <h2 className='standalone-warning__subtitle'>Add To Home Screen</h2>
+  //             <small>از منو ظاهر شده این گزینه را پیدا و انتخاب کنید</small>
+  //           </div>
+  //         </div>
+  //         <div className='standalone-warning__box'>
+  //           <Smartphone className='standalone-warning__icon' />
+  //           <div className='standalone-warning__text'>
+  //             <h2 className='standalone-warning__subtitle'>Add</h2>
+  //             <small>بدون تغییر هیچ گزینه دکمه Add را بزنید</small>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+
 
   return (
     <div className='container'>
