5b91cdca03b4215e30d70ce6fcf55bb11650b3ae
mani rahmati <astrroeffect@gmail.com>
2025-10-19 02:41:51 +0330

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate, useRoutes } from 'react-router-dom'
 import { ToastContainer, Bounce } from 'react-toastify';
 import { Share, Smartphone, SquarePlus } from 'lucide-react';
-import { requestPermission } from "./firebase";
 import Cookies from 'js-cookie';
 import Navbar from './components/Navbar';
 import routes from './router';
@@ -15,10 +14,6 @@ export default function App() {
     const location = useLocation();
     const navigate = useNavigate();
     const showNavbar = location.pathname !== '/login';
-
-    useEffect(() => {
-        requestPermission();
-    }, []);
 
     useEffect(() => {
 
@@ -40,37 +35,37 @@ export default function App() {
 
     }, [location, navigate]);
 
-    // if (!isStandalone) {
-    //     return (
-    //         <div className='standalone-warning'>
-    //             <img src={`${process.env.PUBLIC_URL}/logo.png`} className='standalone-warning__logo' alt="" />
-    //             <h1 className='standalone-warning__title'>وب اپلیکیشن را دانلود کنید</h1>
-    //             <div className='standalone-warning__details'>
-    //                 <div className='standalone-warning__box'>
-    //                     <Share className='standalone-warning__icon' />
-    //                     <div className='standalone-warning__text'>
-    //                         <h2 className='standalone-warning__subtitle'>Share</h2>
-    //                         <small>گزینه share را از نوار مرورگر خود انتخاب کنید</small>
-    //                     </div>
-    //                 </div>
-    //                 <div className='standalone-warning__box'>
-    //                     <SquarePlus className='standalone-warning__icon' />
-    //                     <div className='standalone-warning__text'>
-    //                         <h2 className='standalone-warning__subtitle'>Add To Home Screen</h2>
-    //                         <small>از منو ظاهر شده این گزینه را پیدا و انتخاب کنید</small>
-    //                     </div>
-    //                 </div>
-    //                 <div className='standalone-warning__box'>
-    //                     <Smartphone className='standalone-warning__icon' />
-    //                     <div className='standalone-warning__text'>
-    //                         <h2 className='standalone-warning__subtitle'>Add</h2>
-    //                         <small>بدون تغییر هیچ گزینه دکمه Add را بزنید</small>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     );
-    // }
+    if (!isStandalone) {
+        return (
+            <div className='standalone-warning'>
+                <img src={`${process.env.PUBLIC_URL}/logo.png`} className='standalone-warning__logo' alt="" />
+                <h1 className='standalone-warning__title'>وب اپلیکیشن را دانلود کنید</h1>
+                <div className='standalone-warning__details'>
+                    <div className='standalone-warning__box'>
+                        <Share className='standalone-warning__icon' />
+                        <div className='standalone-warning__text'>
+                            <h2 className='standalone-warning__subtitle'>Share</h2>
+                            <small>گزینه share را از نوار مرورگر خود انتخاب کنید</small>
+                        </div>
+                    </div>
+                    <div className='standalone-warning__box'>
+                        <SquarePlus className='standalone-warning__icon' />
+                        <div className='standalone-warning__text'>
+                            <h2 className='standalone-warning__subtitle'>Add To Home Screen</h2>
+                            <small>از منو ظاهر شده این گزینه را پیدا و انتخاب کنید</small>
+                        </div>
+                    </div>
+                    <div className='standalone-warning__box'>
+                        <Smartphone className='standalone-warning__icon' />
+                        <div className='standalone-warning__text'>
+                            <h2 className='standalone-warning__subtitle'>Add</h2>
+                            <small>بدون تغییر هیچ گزینه دکمه Add را بزنید</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className='container'>
